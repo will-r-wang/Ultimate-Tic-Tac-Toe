4ebc2b66cf5870149cd43c856fdc2ff20820bba0
sayohnahilan <sayohnahilan@gmail.com>
2021-01-15 15:00:32 -0500

    reset_game

--- game.rb
+++ game.rb
@@ -4,8 +4,6 @@ module UltimateTicTacToe
   class Game
     class InvalidMoveError < StandardError; end
 
-    attr_reader :board
-
     ROWS, COLS = 9, 9
     WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
