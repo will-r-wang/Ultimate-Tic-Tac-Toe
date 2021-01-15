70ea97a9b099710252888785ba14c0bdfac1f133
sayohnahilan <sayohnahilan@gmail.com>
2021-01-15 18:41:46 -0500

    final?

--- markdown_generator.rb
+++ markdown_generator.rb
@@ -1,8 +1,9 @@
 require_relative 'game'
 
 class MarkdownGenerator
-
+  ISSUE_BASE_URL = 'https://github.com/sayohnahilan/Ultimate-Tic-Tac-Toe/issues/new'
   IMAGE_BASE_URL = 'https://github.com/sayohnahilan/Ultimate-Tic-Tac-Toe/blob/main/images'
+  ISSUE_BODY = 'body=Just+push+%27Submit+new+issue%27+without+editing+the+title.+The+README+will+be+updated+after+approximately+30+seconds.'
 
   O_IMAGE = "![](#{IMAGE_BASE_URL}/o.png)"
   X_IMAGE = "![](#{IMAGE_BASE_URL}/x.png)"
@@ -26,11 +27,14 @@ class MarkdownGenerator
 
   def generate_game_board
     headers = ' |0|1|2|3|4|5|6|7|8'
-    game_board = "|#{headers}|\n| - | - | - | - | - | - | - | - | - | - |\n"
+    game_board = "|#{headers}|\n|-|-|-|-|-|-|-|-|-|-|\n"
 
     0.upto(8) do |row|
       format = (0...9).map do |col|
-        if game.board[row][col] == 'X'
+
+        if game.valid_moves.include?("#{row}|#{col}")
+          "![move](#{ISSUE_BASE_URL}?title=move%7C#{row}%7C#{col}&#{ISSUE_BODY})"
+        elsif game.board[row][col] == 'X'
           X_IMAGE
         elsif game.board[row][col] == 'O'
           O_IMAGE
