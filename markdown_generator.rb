bac7d4c89741d1c323093dc46e9b4d4401f4cb04
William Wang <william.wang@shopify.com>
2021-01-15 20:17:45 -0500

    Final touchups
    Minor style and wording tweaks.

--- markdown_generator.rb
+++ markdown_generator.rb
@@ -2,12 +2,7 @@ require_relative 'game'
 
 class MarkdownGenerator
   ISSUE_BASE_URL = 'https://github.com/will-r-wang/Ultimate-Tic-Tac-Toe/issues/new'
-  IMAGE_BASE_URL = 'https://github.com/will-r-wang/Ultimate-Tic-Tac-Toe/blob/main/images'
-  ISSUE_BODY = 'body=Just+push+%27Submit+new+issue%27+without+editing+the+title.+The+README+will+be+updated+after+approximately+30+seconds.'
-
-  O_IMAGE = "![](#{IMAGE_BASE_URL}/o.png)"
-  X_IMAGE = "![](#{IMAGE_BASE_URL}/x.png)"
-  EMPTY_IMAGE = "![](#{IMAGE_BASE_URL}/empty.png)"
+  ISSUE_BODY = 'body=Just+push+%27Submit+new+issue%27+without+modifying+the+title.+The+README+will+be+updated+after+approximately+30+seconds.'
 
   def initialize(game:)
     @game = game
@@ -15,16 +10,22 @@ class MarkdownGenerator
 
   def readme
     markdown = <<~HTML
-        ## :game_die: Welcome to our community Ultimate Tic Tac Toe game! 👋
-        Everyone is welcome to participate! To make a move, click on the board where valid.
-    HTML
+        ## Community Ultimate Tic Tac Toe
+        Welcome to our community game of Ultimate Tic Tac Toe!
 
+        ### Rules:
+        Win three games of Tic Tac Toe in a row. You may only play  
+        in the big field that corresponds to the last small field your  
+        opponent played. When your are sent to a field that is already  
+        decided, you can choose freely. [\[1\]](https://bejofo.net/ttt)  
+
+        Click on a 👾 and press submit issue to make a move.  
+    HTML
+    markdown.concat("\n\nNext to move: #{@game.turn.even? ? '❌' : '⭕️'}'s turn.")
     markdown.concat(generate_game_board)
-    markdown.concat("\nInspired by: [Community Connect Four Game!](https://github.com/JonathanGin52/JonathanGin52/)")
+    markdown.concat("\nInspired by: [Community Connect Four Game!](https://github.com/JonathanGin52/JonathanGin52/) - [@JonathanGin52](https://github.com/jonathangin52)")
     markdown
   end
-
-  attr_reader :game
 
   def generate_game_board
     headers = ' |0|1|2|3|4|5|6|7|8'
@@ -32,14 +33,14 @@ class MarkdownGenerator
 
     0.upto(8) do |row|
       format = (0...9).map do |col|
-        if game.valid_moves.include?("#{row}|#{col}")
+        if @game.valid_moves.include?("#{row}|#{col}")
           "[move](#{ISSUE_BASE_URL}?title=move%7C#{row}%7C#{col}&#{ISSUE_BODY})"
-        elsif game.board[row][col] == 'X'
-          X_IMAGE
-        elsif game.board[row][col] == 'O'
-          O_IMAGE
+        elsif @game.board[row][col] == 'X'
+          ❌
+        elsif @game.board[row][col] == 'O'
+          ⭕️
         else
-          EMPTY_IMAGE
+          ' '
         end
       end
       game_board.concat("|#{row}|#{format.join('|')}|\n")
